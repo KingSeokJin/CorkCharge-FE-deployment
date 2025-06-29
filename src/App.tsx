533d602e3f7edc306618927330094415ac8182f3
KingSeokJin <sjin7629@gmail.com>
2025-06-30 02:37:37 +0900

    [feat] tailwind 자동완성 기능 및 프리티어 설정 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,27 @@
-import { useState } from 'react'
-import reactLogo from '../public/vite.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
- <div className="min-h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white flex flex-col items-center justify-center">
-      <div className="flex gap-4 mb-6">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="h-16 hover:scale-110 transition" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="h-16 hover:scale-110 transition" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-4xl font-bold mb-4">Vite + React + Tailwind 🎉</h1>
-      <div className="bg-white text-black rounded shadow p-6">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white">
+      <h1 className="mb-4 text-4xl font-bold">Vite + React + Tailwind 🎉</h1>
+      <div className="rounded bg-white p-6 text-black shadow">
         <button
           onClick={() => setCount((count) => count + 1)}
-          className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition"
+          className="rounded bg-red-600 px-4 py-2 text-white transition hover:bg-blue-700"
         >
           count is {count}
         </button>
-        <p className="mt-2 text-sm bg-[red]">
+        <p className="mt-2 bg-[red] text-sm">
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="mt-4 text-sm italic">Click on the logos to learn more</p>
+      <p className="bg-[blue] text-xl">내 이름은 김석진이야</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
